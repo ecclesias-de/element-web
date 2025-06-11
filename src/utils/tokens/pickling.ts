f7fd6cc26b35bc676434ae9094319e29ee09c3b2
Milan Mertens <m.mertens@metaways.de>
2025-06-11 14:14:47 +0200

    tine integration: session key

--- src/utils/tokens/pickling.ts
+++ src/utils/tokens/pickling.ts
@@ -83,6 +83,20 @@ export async function encryptPickleKey(
 
     const additionalData = getPickleAdditionalData(userId, deviceId);
     const encrypted = await crypto.subtle.encrypt({ name: "AES-GCM", iv, additionalData }, cryptoKey, pickleKey);
+
+    // TINE INTEGRATION - bind pickle key to session - PATCH START
+    // We encrypt the pickleKey a second time, with a key stored in session store. This "locks" element when the session store is cleared e.g.
+    // the browser is closed. The tine session key is store in tine. Our wrapper can set it again to resume the "session".
+    // Adding a second layer of encryption, should not weaken the previous assumptions.
+    const sessionKeyEncoded = sessionStorage.getItem('tine_session_encryption_key')
+    if (sessionKeyEncoded) {
+        const sessionKey = await crypto.subtle.importKey("raw", Uint8Array.fromBase64!(sessionKeyEncoded), "AES-GCM", false, ["encrypt", "decrypt"]);
+
+        const encrypted2 = await crypto.subtle.encrypt({ name: "AES-GCM", iv }, sessionKey, encrypted);
+
+        return { encrypted: encrypted2, iv, cryptoKey }
+    }
+    // TINE INTEGRATION - PATCH END
     return { encrypted, iv, cryptoKey };
 }
 
@@ -111,6 +125,15 @@ export async function buildAndEncodePickleKey(
     }
 
     try {
+        // TINE INTEGRATION - bind pickle key to session - PATCH START
+        // see comment in encryptPickleKey patch
+        const sessionKeyEncoded = sessionStorage.getItem('tine_session_encryption_key')
+        if (sessionKeyEncoded) {
+            const sessionKey = await crypto.subtle.importKey("raw", Uint8Array.fromBase64!(sessionKeyEncoded), "AES-GCM", false, ["encrypt", "decrypt"]);
+
+            data.encrypted = await crypto.subtle.decrypt({ name: "AES-GCM", iv: data.iv }, sessionKey, data.encrypted);
+        }
+        // TINE INTEGRATION - PATCH END
         const additionalData = getPickleAdditionalData(userId, deviceId);
         const pickleKeyBuf = await crypto.subtle.decrypt(
             { name: "AES-GCM", iv: data.iv, additionalData },
--- src/vector/tine.ts
+++ src/vector/tine.ts
@@ -1,5 +1,13 @@
 // notes on post message security: This script should not need to know origin of embedding page in advanced. Therefore
 // the user data request must be send to all origins. The origin of the first successfully handled response, will become the
+
+import { persistAccessTokenInStorage } from "../utils/tokens/tokens";
+import WebPlatform from "./platform/WebPlatform";
+import createMatrixClient from "../utils/createMatrixClient";
+import EventIndexPeg from "../indexing/EventIndexPeg";
+import * as StorageAccess from "../utils/StorageAccess";
+
+
 // new origin. It is stored in window.localStorage["tine_origin"].
 export async function tineBootstrap(start: () => Promise<void>) {
     window.localStorage.clear();
@@ -41,20 +49,49 @@ let recoveryKey: string | undefined = undefined
 
 
 async function onElementUserdataResponse(event: MessageEvent<any>, start: () => Promise<void>) {
+    // await clearStorage()
     // todo check if another user is singed in, and then handle that.
+
+    // console.log("cryptooooooo: ", window.crypto.getRandomValues(new Uint8Array(32)).toBase64!())
+    window.sessionStorage.setItem("tine_session_encryption_key", event.data.session_key)
 
     window.localStorage.setItem("mx_hs_url", event.data.mx_hs_url)
     window.localStorage.setItem("mx_is_url", event.data.mx_is_url)
     window.localStorage.setItem("mx_user_id", event.data.mx_user_id)
     window.localStorage.setItem("mx_device_id", event.data.mx_device_id)
-    window.localStorage.setItem("mx_access_token", event.data.mx_access_token)
-    window.localStorage.setItem("mx_has_access_token", String(true))
+
+    const platform = new WebPlatform()
+    let pickleKey = await platform.getPickleKey(event.data.mx_user_id, event.data.mx_device_id)
+    if (!pickleKey) {
+        pickleKey = await platform.createPickleKey(event.data.mx_user_id, event.data.mx_device_id)
+    }
+
+    persistAccessTokenInStorage(event.data.mx_access_token, pickleKey!)
 
     recoveryPassword = event.data.recovery_password
     recoveryKey = event.data.recovery_key
 
     console.info("Starting element.");
     start()
+}
+
+// based on Lifecycle clearStorage
+async function clearStorage() {
+    window.localStorage.clear();
+    try {
+        await StorageAccess.idbClear("account");
+    } catch (e) {
+        console.error("idbClear failed for account", e);
+    }
+    window.sessionStorage?.clear();
+
+    const cli = createMatrixClient({
+        // we'll never make any requests, so can pass a bogus HS URL
+        baseUrl: "",
+    });
+
+    await EventIndexPeg.deleteEventIndex();
+    await cli.clearStores();
 }
 
 export function getRecoveryData(): {passphrase: string | undefined, recoveryKey: string | undefined} {
