31ec01ee5370bf7adaf4d62a716fcddeacd777fd
Milan Mertens <m.mertens@metaways.de>
2025-10-01 17:24:44 +0200

    tine integration: notifiy tine if element open in another tab

--- apps/web/src/components/structures/MatrixChat.tsx
+++ apps/web/src/components/structures/MatrixChat.tsx
@@ -141,6 +141,7 @@ import Markdown from "../../Markdown";
 import { LinkedTextConfiguration, sanitizeHtmlParams } from "../../Linkify";
 import { isOnlyAdmin } from "../../utils/membership";
 import { ModuleApi } from "../../modules/Api.ts";
+import { onElementOpenInAnotherWindow } from "../../vector/tine";
 
 // legacy export
 export { default as Views } from "../../Views";
@@ -2182,6 +2183,9 @@ export default class MatrixChat extends React.PureComponent<IProps, IState> {
                 </div>
             );
         } else if (this.state.view === Views.CONFIRM_LOCK_THEFT) {
+            //TINE INTEGRATION - PATCH START
+            onElementOpenInAnotherWindow()
+            //TINE INTEGRATION - PATCH END
             view = (
                 <ConfirmSessionLockTheftView
                     onConfirm={() => {
