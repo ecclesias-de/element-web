96f53b68f9d1fe8a472008e07b09247f008f8d68
Milan Mertens <m.mertens@metaways.de>
2025-11-05 14:52:47 +0100

    tine-integration: change default chat layout to bubbles
    RT#256307

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -1238,7 +1238,7 @@ export const SETTINGS: Settings = {
     },
     "layout": {
         supportedLevels: LEVELS_ACCOUNT_SETTINGS,
-        default: Layout.Group,
+        default: Layout.Bubble,
     },
     "Images.size": {
         supportedLevels: LEVELS_ACCOUNT_SETTINGS,
